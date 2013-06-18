91f292d763e80744b258786d72de8dc54bd491a9
Ivan Glushkov <gli@gmail.com>
2013-06-18 17:25:46 +0400

    add replayer_controller to supervisor

--- src/event_replayer_sup.erl
+++ src/event_replayer_sup.erl
@@ -25,6 +25,8 @@ start_link() ->
 
 init([]) ->
     {ok, { {one_for_one, 5, 10}, [
-        ?CHILD(tasks_generator, worker)
+        ?CHILD(tasks_generator, worker),
+        {replayer_controller, {replayer_controller, start_link, [[]]},
+            permanent, 5000, worker, [replayer_controller]}
             ]} }.
 
