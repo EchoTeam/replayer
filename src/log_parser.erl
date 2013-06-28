ad094d4f43d42a17b121f2add09a2da61a861aa0
Ivan Glushkov <gli@gmail.com>
2013-06-28 15:35:37 +0400

    rename the behavior

---
+++ src/log_parser.erl
@@ -0,0 +1,8 @@
+-module(log_processor).
+-export([behaviour_info/1]).
+
+% reader function should be of type
+% -type continuation_fun() :: fun(() -> eof | {continuation_fun(), [replayer_utils:request()]}).
+% -spec reader(File :: string()) -> {continuation_fun(), [replayer_utils:request()]}.
+behaviour_info(callbacks) -> [{reader, 1}];
+behaviour_info(_Other) -> undefined.
